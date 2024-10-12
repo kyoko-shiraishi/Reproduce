8a01475b7f1c6e9e32d492516e1174ae949bcd58
kyoko-shiraishi <kyon0704@docomo.ne.jp>
2024-10-13 03:10:19 +0900

    新規スレッド投稿

--- resources/views/please/new_thread_create.blade.php
+++ resources/views/please/new_thread_create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>新規スレッド作成ページ</title>
+    <!-- 新規スレッド作成ページ -->
 </head>
 <body>
     <h1>新規スレッド作成ページ</h1>  
