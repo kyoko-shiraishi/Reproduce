b33a5402ede0b6d9b7d810b512c33cee4e3f2873
kyoko-shiraishi <kyon0704@docomo.ne.jp>
2024-11-02 15:35:14 +0900

    devide local and production

---
+++ .history/app/Providers/AppServiceProvider_20241031205708.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category; // Categoryモデルをインポート
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        \URL::forceScheme('https');  
+        $this->app['request']->server->set('HTTPS','on');
+        // すべてのビューにカテゴリを渡す
+        View::composer('*', function ($view) {
+            $categories = Category::all();
+        
+            $view->with('categories', $categories);
+        });
+    }
+
+    public function register()
+    {
+        //
+    }
+}
