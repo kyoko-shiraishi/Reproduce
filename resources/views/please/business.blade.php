a017e5f8a86ceef3c76de3d8b79630264f413e92
kyoko-shiraishi <kyon0704@docomo.ne.jp>
2024-11-23 20:54:46 +0900

    installed phpspreadsheet

--- resources/views/please/business.blade.php
+++ resources/views/please/business.blade.php
@@ -1,13 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>business</title>
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="header">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <p>検索してね</p>
+            <form action="{{ route('search.results') }}" method="GET">
+                @csrf
+                <input type="text" id="company_name" name="company_name" value="{{ old('company_name') }}" placeholder="会社名">
+                <input type="text" name="product_name" value="{{ old('product_name') }}" placeholder="製品名">
 
-    <h1>企業向けマーケページ</h1>
-</body>
-</html>
+
+                <select name="category_id">
+                    <option value="">すべてのカテゴリ</option>
+                    @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+
+                <button class="bg-gray-500 hover:bg-gray-400 text-white rounded px-4 py-2 button" type="submit">検索</button>
+
+            </form>
+        </h2>
+    </x-slot>
+</x-app-layout>
